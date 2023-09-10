b6f1c50371e1e9f15c6090f1770e30dae3eff385
Matthew Evans <mcaei@fastmail.com>
2023-09-10 17:58:36 -0600

    Fix whitespace and try to initiate a CI build...

--- src/state.test.ts
+++ src/state.test.ts
@@ -4,11 +4,13 @@ import { Citizen, Species } from "./citizens";
 
 test("empty state returns 0 population", () => {
   const state = State.empty();
+
   expect(state.population()).toBe(0);
 });
 
 test("base state returns 20 population", () => {
   const state = State.seed();
+
   expect(state.population()).toBe(20);
 });
 
@@ -25,18 +27,17 @@ test("adds and retrieves citizens", () => {
   });
 
   const citizens = state.getCitizens();
+  const citizen = state.getCitizen("123");
 
   expect(citizens.length).toBe(1);
   expect(citizens[0].id).toBe("123");
-
-  const citizen = state.getCitizen("123");
-
   expect(citizen).toBeTruthy();
 });
 
 test("sets and retrieves citizens", () => {
   const state = State.empty();
   const citizens = new Map<string, Citizen>();
+
   citizens.set("123", {
     alive: false,
     height: 0,
@@ -46,5 +47,6 @@ test("sets and retrieves citizens", () => {
     weight: 0,
   });
   state.setCitizens(citizens);
+
   expect(state.population()).toBe(1);
 });
