60943d3fcb26a3411c70f13b3e7fe047685c118b
Matthew Evans <mcaei@fastmail.com>
2023-09-28 22:35:52 -0600

    Complain about the State type

--- src/system.ts
+++ src/system.ts
@@ -4,4 +4,7 @@ import { WorldTimeState } from "./system/worldTime";
 export interface System {
   tick(delta: number, global: State[]): State;
 }
+
+// The maintenance of having to add State interfaces to this type is a little annoying,
+// but not too bad in the grand scheme of things.
 export type State = WorldTimeState | CitizensState;
