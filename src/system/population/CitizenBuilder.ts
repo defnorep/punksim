396729020edf19a9045ed62642e461608b3fc48b
Matthew Evans <mcaei@fastmail.com>
2023-10-15 12:41:00 -0600

    Extract a CitizenBuilder
    It's an interesting flavour with a randomizer built in.

---
+++ src/system/population/CitizenBuilder.ts
@@ -0,0 +1,253 @@
+import { randomBytes, randomInt } from "crypto";
+import modifiers from "../../../data/modifiers.json";
+import names from "../../../data/names.json";
+import { Component } from "../../ecs";
+import { Kilograms, LocationComponent, Meters } from "../global";
+import {
+  CivicIdentityComponent,
+  EpochComponent,
+  Gender,
+  GenderComponent,
+  Implant,
+  ImplantsComponent,
+  LifeformClassificationComponent,
+  Modifiers,
+  PhysicalComponent,
+  Species,
+  Status,
+  age,
+} from "../population";
+import { LocationId } from "../transport";
+
+export class CitizenBuilder {
+  private components = new Map<Function, Component>();
+  private modifiers: Modifiers = modifiers;
+
+  setGender(gender: Gender) {
+    this.components.set(GenderComponent, new GenderComponent(gender));
+
+    return this;
+  }
+
+  randomGender() {
+    const index = Math.floor(Math.random() * Object.keys(Gender).length);
+    const value = Object.values(Gender)[index];
+
+    const gender = Gender[value];
+
+    this.components.set(GenderComponent, new GenderComponent(gender));
+
+    return this;
+  }
+
+  setSpecies(species: Species) {
+    this.components.set(
+      LifeformClassificationComponent,
+      new LifeformClassificationComponent(species),
+    );
+
+    return this;
+  }
+
+  /**
+   * Civic Identity depends on a Lifeform Classification.
+   * Either it can be set explicitly before randomCivicIdentity,
+   * or it can be called directly from randomCivicIdentity.
+   *
+   * TL;DR this is private to prevent illegal state.
+   */
+  private randomLifeform() {
+    const index = Math.floor(Math.random() * Object.keys(Species).length);
+    const value = Object.values(Species)[index];
+
+    const species = Species[value];
+
+    this.components.set(
+      LifeformClassificationComponent,
+      new LifeformClassificationComponent(species),
+    );
+
+    return this;
+  }
+
+  setCivicIdentity(civicId: string, name: string, surname: string) {
+    this.components.set(
+      CivicIdentityComponent,
+      new CivicIdentityComponent(civicId, name, surname, Status.Living),
+    );
+
+    return this;
+  }
+
+  randomCivicIdentity() {
+    if (!this.components.has(LifeformClassificationComponent)) {
+      this.randomLifeform();
+    }
+
+    const lifeform = this.components.get(
+      LifeformClassificationComponent,
+    ) as LifeformClassificationComponent;
+
+    const names = generateCitizenName(lifeform.species);
+    this.components.set(
+      CivicIdentityComponent,
+      new CivicIdentityComponent(
+        randomBytes(4).toString("hex"),
+        names[0],
+        names[1],
+        Status.Living,
+      ),
+    );
+
+    return this;
+  }
+
+  setAge(birthdate: Date, reference: Date) {
+    this.components.set(
+      EpochComponent,
+      new EpochComponent(age(birthdate, reference), birthdate),
+    );
+
+    return this;
+  }
+
+  randomAge(reference: Date) {
+    const birthdate = new Date();
+    birthdate.setFullYear(
+      reference.getFullYear() - Math.round(Math.random() * 80),
+    );
+    this.components.set(
+      EpochComponent,
+      new EpochComponent(age(birthdate, reference), birthdate),
+    );
+
+    return this;
+  }
+
+  setPhysicalCharacteristics(height: Meters, mass: Kilograms) {
+    this.components.set(
+      PhysicalComponent,
+      new PhysicalComponent([height, 0.6, 0.3], mass),
+    );
+
+    return this;
+  }
+
+  randomPhysicalCharacteristics(
+    averageHeight: number,
+    heightJitter: number,
+    averageMass: number,
+    massJitter: number,
+  ) {
+    this.components.set(
+      PhysicalComponent,
+      new PhysicalComponent(
+        [
+          randomInt(averageHeight - heightJitter, averageHeight + heightJitter),
+          0.6,
+          0.3,
+        ],
+        randomInt(averageMass - massJitter, averageMass + massJitter),
+      ),
+    );
+
+    return this;
+  }
+
+  setLocation(location: LocationId) {
+    this.components.set(LocationComponent, new LocationComponent(location));
+
+    return this;
+  }
+
+  setImplants(implants: Implant[]) {
+    this.components.set(ImplantsComponent, new ImplantsComponent(implants));
+
+    return this;
+  }
+
+  randomImplants() {
+    const implants: Implant[] = [];
+    const implantRoll = randomInt(0, 3);
+    if (implantRoll > 2) {
+      // 25% chance
+      implants.push(
+        this.modifiers.implants[
+          Math.floor(
+            Math.random() * Object.keys(this.modifiers.implants).length,
+          )
+        ],
+      );
+    }
+
+    this.components.set(ImplantsComponent, new ImplantsComponent(implants));
+
+    return this;
+  }
+
+  static random(
+    reference: Date,
+    averageHeight: number,
+    heightJitter: number,
+    averageMass: number,
+    massJitter: number,
+    location: LocationId,
+  ) {
+    return new CitizenBuilder()
+      .randomCivicIdentity()
+      .randomGender()
+      .randomAge(reference)
+      .randomPhysicalCharacteristics(
+        averageHeight,
+        heightJitter,
+        averageMass,
+        massJitter,
+      )
+      .randomImplants()
+      .setLocation(location)
+      .build();
+  }
+
+  build() {
+    return [
+      GenderComponent,
+      CivicIdentityComponent,
+      LifeformClassificationComponent,
+      EpochComponent,
+      PhysicalComponent,
+      LocationComponent,
+      ImplantsComponent,
+    ].map((component) => {
+      const c = this.components.get(component);
+      if (c === undefined) {
+        throw new Error(`Citizen Builder requires ${component.name}`);
+      } else {
+        return c;
+      }
+    });
+  }
+}
+
+const generateCitizenName = (species: Species = Species.Human) => {
+  switch (species) {
+    case Species.Android:
+      return generateAndroidName();
+    case Species.Human:
+    default:
+      return generateHumanName();
+  }
+};
+
+const generateHumanName = (): [string, string] => {
+  return [
+    names.human[Math.floor(Math.random() * names.human.length)],
+    names.human[Math.floor(Math.random() * names.human.length)],
+  ];
+};
+
+const generateAndroidName = (): [string, string] => {
+  return [
+    names.elements[Math.floor(Math.random() * names.elements.length)],
+    randomInt(100, 999).toString(),
+  ];
+};
--- src/system/population/PopulationStartupSystem.ts
+++ src/system/population/PopulationStartupSystem.ts
@@ -1,23 +1,8 @@
-import { randomBytes, randomInt } from "crypto";
-import names from "../../../data/names.json";
 import { Ecs, System } from "../../ecs";
 import { EntityContainer } from "../../ecs/entityContainer";
-import { LocationComponent } from "../global";
-import {
-  CivicIdentityComponent,
-  EpochComponent,
-  Gender,
-  GenderComponent,
-  Implant,
-  ImplantsComponent,
-  LifeformClassificationComponent,
-  Modifiers,
-  PhysicalComponent,
-  Species,
-  Status,
-  age,
-} from "../population";
+import { Modifiers } from "../population";
 import { TimeComponent } from "../time";
+import { CitizenBuilder } from "./CitizenBuilder";
 
 /**
  * The PopulationStartupSystem is responsible for creating the initial set of citizens.
@@ -36,78 +21,16 @@ export class PopulationStartupSystem extends System {
     Array(this.population)
       .fill(1)
       .forEach(() => {
-        const isAndroid = Math.random() > 0.7;
-        const species = isAndroid ? Species.Android : Species.Human;
-        const names = generateCitizenName(species);
-        const birthdate = new Date();
-        birthdate.setFullYear(
-          time.datetime.getFullYear() - Math.round(Math.random() * 80),
+        this.ecs.createEntity(
+          ...CitizenBuilder.random(
+            time.datetime,
+            1.8,
+            0.2,
+            80,
+            10,
+            "Residence-1",
+          ),
         );
-
-        const genderRoll = randomInt(0, 3);
-        let gender = Gender.None;
-        if (genderRoll > 0) {
-          gender = Gender.Male;
-          if (genderRoll > 1) {
-            gender = Gender.Female;
-          }
-        }
-
-        const implants: Implant[] = [];
-        const implantRoll = randomInt(0, 3);
-        if (implantRoll > 2) {
-          // 25% chance
-          implants.push(
-            this.modifiers.implants[
-              Math.floor(
-                Math.random() * Object.keys(this.modifiers.implants).length,
-              )
-            ],
-          );
-        }
-
-        const citizen = [
-          new CivicIdentityComponent(
-            randomBytes(4).toString("hex"),
-            names[0],
-            names[1],
-            Status.Living,
-          ),
-          new EpochComponent(age(birthdate, time.datetime), birthdate),
-          new PhysicalComponent(
-            [randomInt(150, 190) / 10, 0.6, 0.3],
-            randomInt(60, 90),
-          ),
-          new LifeformClassificationComponent(species),
-          new GenderComponent(gender),
-          new LocationComponent("Residence-1"),
-          new ImplantsComponent(implants),
-        ];
-        this.ecs.createEntity(...citizen);
       });
   }
 }
-
-export const generateCitizenName = (species: Species = Species.Human) => {
-  switch (species) {
-    case Species.Android:
-      return generateAndroidName();
-    case Species.Human:
-    default:
-      return generateHumanName();
-  }
-};
-
-const generateHumanName = (): [string, string] => {
-  return [
-    names.human[Math.floor(Math.random() * names.human.length)],
-    names.human[Math.floor(Math.random() * names.human.length)],
-  ];
-};
-
-const generateAndroidName = (): [string, string] => {
-  return [
-    names.elements[Math.floor(Math.random() * names.elements.length)],
-    randomInt(100, 999).toString(),
-  ];
-};
